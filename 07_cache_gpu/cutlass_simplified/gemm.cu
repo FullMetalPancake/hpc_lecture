1e961eb0f6ddfe5f3d9b8ed08d3a512bd6227f7c
fullmetalpancake <ge.s.aa@m.titech.ac.jp>
2020-06-04 05:26:30 +0900

    Replace matrix with native arrays

--- 07_cache_gpu/cutlass_simplified/gemm.cu
+++ 07_cache_gpu/cutlass_simplified/gemm.cu
@@ -7,10 +7,38 @@
 
 #include <gemm/dispatch.h>
 #include <gemm/epilogue_function.h>
-#include "util/matrix.h"
 #include "util/timer.h"
 
 using namespace cutlass;
+
+float random(float *M, int m, int n) {
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+      (&M[0])[i + j * m] = float(drand48());
+    }
+  }
+}
+
+float zeros(float *M, int m, int n) {
+  for (int j = 0; j < n; j++) {
+    for (int i = 0; i < m; i++) {
+      (&M[0])[i + j * m] = 0.0;
+    }
+  }
+}
+
+void sync_device(float *M, float *d_data, int m, int n)
+{
+  size_t bytes = m * n * sizeof(float);
+  CUDA_PERROR_EXIT(cudaMemcpy(d_data, &M[0], bytes, cudaMemcpyHostToDevice));
+}
+
+void sync_host(float *M, float *d_data, int m, int n)
+{
+    size_t bytes = m * n * sizeof(float);
+    CUDA_PERROR_EXIT(cudaMemcpy(&M[0], d_data, bytes, cudaMemcpyDeviceToHost));
+}
+
 
 int main(int argc, const char **argv) {
   int m = 10240;
@@ -20,22 +48,20 @@ int main(int argc, const char **argv) {
   float beta = 0.0;
   static const matrix_transform_t::kind_t TransformA = matrix_transform_t::NonTranspose;
   static const matrix_transform_t::kind_t TransformB = matrix_transform_t::NonTranspose;
-  typedef float value_t;
-  typedef float accum_t;
   int g_timing_iterations = 10;
   cudaStream_t stream = 0;
-  matrix<value_t> A(m, k);
-  matrix<value_t> B(k, n);
-  matrix<accum_t> C(m, n);
-  matrix<accum_t> C2(m, n);
-  A.random();
-  B.random();
-  C.fill_ramp(0,0);
-  C2.fill_ramp(0,0);
-  A.sync_device();
-  B.sync_device();
-  C.sync_device();
-  C2.sync_device();
+  float A[m * k]; random(A, m, k);
+  float B[k * n]; random(B, k, n);
+  float C[m * n]; zeros(C, m, n);
+  float C2[m * n]; zeros(C2, m, n);
+  float *d_A; CUDA_PERROR_EXIT(cudaMalloc((void ** )&d_A, sizeof(float) * m * k));
+  float *d_B; CUDA_PERROR_EXIT(cudaMalloc((void ** )&d_B, sizeof(float) * k * n));
+  float *d_C; CUDA_PERROR_EXIT(cudaMalloc((void ** )&d_C, sizeof(float) * m * n));
+  float *d_C2; CUDA_PERROR_EXIT(cudaMalloc((void ** )&d_C2, sizeof(float) * m * n));
+  sync_device(A, d_A, m, k);
+  sync_device(B, d_B, k, n);
+  sync_device(C, d_C, m, n);
+  sync_device(C2, d_C2, m, n);
   cublasHandle_t g_cublas_handle;
   cublasCreate(&g_cublas_handle);
   gpu_timer timer;
@@ -49,12 +75,12 @@ int main(int argc, const char **argv) {
                             n,
                             k,
                             &alpha,
-                            A.d_data(),
+                            &d_A[0],
                             m,
-                            B.d_data(),
+                            &d_B[0],
                             k,
                             &beta,
-                            C.d_data(),
+                            &d_C[0],
                             m));
   }
   timer.stop();
@@ -71,9 +97,9 @@ int main(int argc, const char **argv) {
         k,
         alpha,
         beta,
-        A.d_data(),
-        B.d_data(),
-        C2.d_data(),
+        d_A,
+        d_B,
+        d_C2,
         stream,
         false);
   }
@@ -81,12 +107,12 @@ int main(int argc, const char **argv) {
   double tcutlass = timer.elapsed_millis() / g_timing_iterations;
   double cutlass_flops = double(num_flops) / tcutlass / 1.0e6;
   printf("CUBLAS: %.2f Gflops, CUTLASS: %.2f Gflops\n", cublas_flops, cutlass_flops);
-  C.sync_host();
-  C2.sync_host();
+  sync_host(C, d_C, m, n);
+  sync_host(C2, d_C2, m, n);
   double err = 0;
   for (int i=0; i<n; i++) {
     for (int j=0; j<m; j++) {
-      err += fabs(C.get(i,j) - C2.get(i,j));
+      err += fabs(C[i + j * n] - C2[i + j * n]);
     }
   }
   printf("error: %lf\n", err/n/m);
