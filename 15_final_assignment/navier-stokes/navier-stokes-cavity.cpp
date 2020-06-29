d6b2fa5fc17324aebe19516a7de77d5ab1a74c0e
stevenge1996 <steven-ge@hotmail.com>
2020-06-30 01:58:22 +0900

    Converted all matrices to vectors

--- 15_final_assignment/navier-stokes/navier-stokes-cavity.cpp
+++ 15_final_assignment/navier-stokes/navier-stokes-cavity.cpp
@@ -14,149 +14,145 @@ using namespace std;
 /*
  * Helper functions
  */
-template<size_t nx, size_t ny>
-void save_matrix_to_csv(double (&arr)[nx][ny], string file_name) {
+template<size_t n>
+void save_matrix_to_csv(double (&arr)[n], string file_name, int nx, int ny) {
 	ofstream file(file_name);
 	for (int i = 0; i < nx; i++) {
 		for (int j = 0; j < ny - 1; j++) {
-			file << arr[i][j] << ',';
+            file << arr[i*nx + j] << ',';
 		}
-		file << arr[i][ny - 1] << '\n';
+        file << arr[i*nx + ny - 1] << '\n';
 	}
 	file.close();
 }
 
-template<size_t nx, size_t ny>
-void copy_matrix(double (&from)[nx][ny], double (&to)[nx][ny]) {
-	for (int i = 0; i < nx; i++) {
-		for (int j = 0; j < ny; j++) {
-			to[i][j] = from[i][j];
-		}
+template<size_t n>
+void copy_matrix(double (&from)[n], double (&to)[n]) {
+	for (int i = 0; i < n; i++) {
+			to[i] = from[i];
 	}
 }
 
-template<size_t size_x, size_t size_y>
-void initialize_matrix(double (&arr)[size_x][size_y]) {
-	for (int i = 0; i < size_x; i++) {
-		for (int j = 0; j < size_y; j++) {
-			arr[i][j] = 0.0;
-		}
+template<size_t n>
+void initialize_matrix(double (&arr)[n]) {
+	for (int i = 0; i < n; i++) {
+        arr[i] = 0.0;
 	}
 }
 
 /*
  * Navier-stokes c++ translation from the provided python code
  */
-template<size_t nx, size_t ny>
-void build_up_b(double (&b)[nx][ny], double rho, double dt, double (&u)[nx][ny],
-		double v[nx][ny], double dx, double dy) {
+template<size_t n>
+void build_up_b(double (&b)[n], double rho, double dt, double (&u)[n],
+		double v[n], double dx, double dy, int nx, int ny) {
 
 	for (int i = 1; i < nx - 1; i++) {
 		for (int j = 1; j < ny - 1; j++) {
-			b[i][j] =
+			b[i*nx + j] =
 					(rho
 							* (1 / dt
-									* ((u[i][j + 1] - u[i][j - 1]) / (2 * dx)
-											+ (v[i + 1][j] - v[i - 1][j])
+									* ((u[i*nx + j + 1] - u[i*nx + j - 1]) / (2 * dx)
+											+ (v[(i + 1)*nx + j] - v[(i - 1)*nx + j])
 													/ (2 * dy))
-									- ((u[i][j + 1] - u[i][j - 1]) / (2 * dx))
-											* ((u[i][j + 1] - u[i][j - 1])
+									- ((u[i*nx + j + 1] - u[i*nx + j - 1]) / (2 * dx))
+											* ((u[i*nx + j + 1] - u[i*nx + j - 1])
 													/ (2 * dx))
 									- 2
-											* ((u[i + 1][j] - u[i - 1][j])
+											* ((u[(i + 1)*nx + j] - u[(i - 1)*nx + j])
 													/ (2 * dy)
-													* (v[i][j + 1] - v[i][j - 1])
+													* (v[i*nx + j + 1] - v[i*nx + j - 1])
 													/ (2 * dx))
-									- ((v[i + 1][j] - v[i - 1][j]) / (2 * dy))
-											* ((v[i + 1][j] - v[i - 1][j])
+									- ((v[(i + 1)*nx + j] - v[(i - 1)*nx + j]) / (2 * dy))
+											* ((v[(i + 1)*nx + j] - v[(i - 1)*nx + j])
 													/ (2 * dy))));
 		}
 	}
 }
 
-template<size_t nx, size_t ny>
-void pressure_poisson(double (&p)[nx][ny], double dx, double dy,
-		double (&b)[nx][ny], int nit) {
-	double pn[nx][ny];
+template<size_t n>
+void pressure_poisson(double (&p)[n], double dx, double dy,
+		double (&b)[n], int nit, int nx, int ny) {
+	double pn[n];
 
 	for (int i = 0; i < nit; i++) {
 		copy_matrix(p, pn);
 		for (int j = 1; j < nx - 1; j++) {
 			for (int k = 1; k < ny - 1; k++) {
-				p[j][k] = (((pn[j][k + 1] + pn[j][k - 1]) * dy * dy
-						+ (pn[j + 1][k] + pn[j - 1][k]) * dx * dx)
+				p[j*nx + k] = (((pn[j*nx + k + 1] + pn[j*nx + k - 1]) * dy * dy
+						+ (pn[(j + 1)*nx + k] + pn[(j - 1)*nx + k]) * dx * dx)
 						/ (2 * (dx * dx + dy * dy))
 						- (dx * dx * dy * dy) / (2 * (dx * dx + dy * dy))
-								* b[j][k]);
+								* b[j*nx + k]);
 			}
 		}
-		for (int i = 0; i < nx; i++) {
-			p[i][ny - 1] = p[i][ny - 2];
-			p[i][0] = p[i][1];
+		for (int j = 0; j < nx; j++) {
+			p[j*nx + ny - 1] = p[j*nx + ny - 2];
+			p[j*nx] = p[j*nx + 1];
 		}
 
 		for (int j = 0; j < ny; j++) {
-			p[0][j] = p[1][j];
-			p[nx - 1][j] = 0;
+			p[j] = p[nx+ j];
+			p[(nx - 1)*nx + j] = 0;
 		}
 	}
 }
 
-template<size_t nx, size_t ny>
-void cavity_flow(int nt, double (&u)[nx][ny], double (&v)[nx][ny], double dt,
-		double dx, double dy, double (&p)[nx][ny], double rho, double nu,
-		int nit) {
-	double un[nx][ny];
-	double vn[nx][ny];
-	double b[nx][ny];
+template<size_t n>
+void cavity_flow(int nt, double (&u)[n], double (&v)[n], double dt,
+		double dx, double dy, double (&p)[n], double rho, double nu,
+		int nit, int nx, int ny) {
+	double un[n];
+	double vn[n];
+	double b[n];
 	initialize_matrix(b);
 
 	for (int i = 0; i < nt; i++) {
 		copy_matrix(u, un);
 		copy_matrix(v, vn);
 
-		build_up_b(b, rho, dt, u, v, dx, dy);
-		pressure_poisson(p, dx, dy, b, nit);
+		build_up_b(b, rho, dt, u, v, dx, dy, nx, ny);
+		pressure_poisson(p, dx, dy, b, nit, nx, ny);
 
 		for (int j = 1; j < nx - 1; j++) {
 			for (int k = 1; k < ny - 1; k++) {
-				u[j][k] = (un[j][k]
-						- un[j][k] * dt / dx * (un[j][k] - un[j][k - 1])
-						- vn[j][k] * dt / dy * (un[j][k] - un[j - 1][k])
-						- dt / (2 * rho * dx) * (p[j][k + 1] - p[j][k - 1])
+				u[j*nx + k] = (un[j*nx + k]
+						- un[j*nx + k] * dt / dx * (un[j*nx + k] - un[j*nx + k - 1])
+						- vn[j*nx + k] * dt / dy * (un[j*nx + k] - un[(j - 1)*nx + k])
+						- dt / (2 * rho * dx) * (p[j*nx + k + 1] - p[j*nx + k - 1])
 						+ nu
 								* (dt / (dx * dx)
-										* (un[j][k + 1] - 2 * un[j][k]
-												+ un[j][k - 1])
+										* (un[j*nx + k + 1] - 2 * un[j*nx + k]
+												+ un[j*nx + k - 1])
 										+ dt / (dy * dy)
-												* (un[j + 1][k] - 2 * un[j][k]
-														+ un[j - 1][k])));
+												* (un[(j + 1)*nx + k] - 2 * un[j*nx + k]
+														+ un[(j - 1)*nx + k])));
 
-				v[j][k] = (vn[j][k]
-						- un[j][k] * dt / dx * (vn[j][k] - vn[j][k - 1])
-						- vn[j][k] * dt / dy * (vn[j][k] - vn[j - 1][k])
-						- dt / (2 * rho * dy) * (p[j + 1][k] - p[j - 1][k])
+				v[j*nx + k] = (vn[j*nx + k]
+						- un[j*nx + k] * dt / dx * (vn[j*nx + k] - vn[j*nx + k - 1])
+						- vn[j*nx + k] * dt / dy * (vn[j*nx + k] - vn[(j - 1)*nx + k])
+						- dt / (2 * rho * dy) * (p[(j + 1)*nx + k] - p[(j - 1)*nx + k])
 						+ nu
 								* (dt / (dx * dx)
-										* (vn[j][k + 1] - 2 * vn[j][k]
-												+ vn[j][k - 1])
+										* (vn[j*nx + k + 1] - 2 * vn[j*nx + k]
+												+ vn[j*nx + k - 1])
 										+ dt / (dy * dy)
-												* (vn[j + 1][k] - 2 * vn[j][k]
-														+ vn[j - 1][k])));
+												* (vn[(j + 1)*nx + k] - 2 * vn[j*nx + k]
+														+ vn[(j - 1)*nx + k])));
 			}
 		}
 		for (int j = 0; j < nx; j++) {
-			u[j][0] = 0;
-			u[j][ny - 1] = 0;
-			v[j][0] = 0;
-			v[j][ny - 1] = 0;
+			u[j*nx] = 0;
+			u[j*nx + ny - 1] = 0;
+			v[j*nx] = 0;
+			v[j*nx + ny - 1] = 0;
 		}
 
 		for (int j = 0; j < ny; j++) {
-			u[0][j] = 0;
-			u[nx - 1][j] = 1;
-			v[0][j] = 0;
-			v[nx - 1][j] = 0;
+			u[j] = 0;
+			u[(nx - 1)*nx + j] = 1;
+			v[j] = 0;
+			v[(nx - 1)*nx + j] = 0;
 		}
 	}
 }
@@ -173,10 +169,10 @@ int main() {
 	const double nu = 0.1;
 	const double dt = 0.001;
 
-	double u[nx][ny];
-	double v[nx][ny];
-	double p[nx][ny];
-	double b[nx][ny];
+	double u[nx*ny];
+	double v[nx*ny];
+	double p[nx*ny];
+	double b[nx*ny];
 
 	initialize_matrix(u);
 	initialize_matrix(b);
@@ -184,16 +180,16 @@ int main() {
 	initialize_matrix(b);
 
 	auto start = std::chrono::high_resolution_clock::now();
-	cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu, nit);
+	cavity_flow(nt, u, v, dt, dx, dy, p, rho, nu, nit, nx, ny);
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>( stop - start ).count();
     ofstream file("result/milliseconds");
 	file << duration;
 	file.close();
 
-	save_matrix_to_csv(u, "result/u.csv");
-	save_matrix_to_csv(v, "result/v.csv");
-	save_matrix_to_csv(p, "result/p.csv");
+	save_matrix_to_csv(u, "result/u.csv", nx, ny);
+	save_matrix_to_csv(v, "result/v.csv", nx, ny);
+	save_matrix_to_csv(p, "result/p.csv", nx, ny);
 	return 0;
 
 }
